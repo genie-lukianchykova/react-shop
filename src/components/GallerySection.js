075b3e1fa6fdc4057a9edf75e9bd56eafacbf3a9
Yevheniia <genie.lukianchykova@gmail.com>
2025-07-27 19:56:28 +0200

    Fix code styling

--- src/components/GallerySection.js
+++ src/components/GallerySection.js
@@ -38,11 +38,7 @@ export default function GallerySection() {
                 </Carousel.Item>
             )
         })
-      }  
-      
-      
-        
-        
+      }    
     </Carousel>
   )
 }
